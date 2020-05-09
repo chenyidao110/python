b9ff950aca14771715b0f56f1bd4bf5786a5d2c2
408624578@qq.com <xiaobai@DESKTOP-1ODIMFE.localdomain>
2020-05-09 14:15:16 +0800

    add shaizi.py

---
+++ day011/shaizi.py
@@ -0,0 +1,10 @@
+import random
+
+counters = [0] * 6
+for _ in range(6000):
+    face = random.randint(1,6)
+    counters[face - 1] += 1
+for face in range(1,7):
+    print(f'{face} appears {counters[face - 1]} times')
+
+print(counters)
