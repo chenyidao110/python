422e2c6a0ae53680ea9ae8889dc5079169062354
408624578@qq.com <xiaobai@DESKTOP-1ODIMFE.localdomain>
2020-05-09 16:29:18 +0800

    add day013

---
+++ day013/scoresAndAvg.py
@@ -0,0 +1,14 @@
+"""
+录入5个学生3门课程的考试成绩
+计算每个学生的平均分和每门课的平均分
+"""
+names = ['关羽','张飞','赵云','马超','黄忠']
+courses = ['语文','数学','英语']
+
+scores = [[0] * len(courses) for _ in range(len(names))]
+
+for i,name in enumerate(names):
+    print(f'请输入{name}的成绩 ===>')
+    for j,course in enumerate(courses):
+        scores[i][j] = float(input(f'{course}:'))
+print()
