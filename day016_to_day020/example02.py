d8ea55dfdb0a46f241e45e8ef42747355b2b4008
chenyidao110 <408624578@qq.com>
2020-06-03 20:47:44 +0800

    add example0{2-3}.py

---
+++ day016_to_day020/example02.py
@@ -0,0 +1,8 @@
+names = ['关羽', '张飞', '赵云', '马超', '黄忠']
+courses = ['语文', '数学', '英语']
+
+scores = [[None] * len(courses) for _ in range(len(names))]
+for row, name in enumerate(names):
+    for col, course in enumerate(courses):
+        scores[row][col] = float(input(f'请输入{name}的成绩：'))
+        print(scores)
